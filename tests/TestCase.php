090e9143998045eb18f7d63df1eeaa805981b005
Maurizio <maurizio.lepora@gmail.com>
2021-04-24 18:25:54 +0200

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,35 +2,15 @@
 
 namespace Tests;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Lemaur\CmsNova\CmsNovaServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-
-        Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Spatie\\CmsNova\\Database\\Factories\\'.class_basename($modelName).'Factory'
-        );
-    }
-
     protected function getPackageProviders($app)
     {
         return [
             CmsNovaServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
-
-        /*
-        include_once __DIR__.'/../database/migrations/create_laravel_cms_nova_table.php.stub';
-        (new \CreatePackageTable())->up();
-        */
     }
 }
